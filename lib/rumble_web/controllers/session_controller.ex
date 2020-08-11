d433ef3feba0c1e7a71e004fcf85a17901ce9afa
DoWhatThouWilt <qhorizon@gmail.com>
2020-08-10 20:51:52 -0400

    "last point before the book tutorial starts using generators"

---
+++ lib/rumble_web/controllers/session_controller.ex
@@ -0,0 +1,28 @@
+defmodule RumbleWeb.SessionController do
+  use RumbleWeb, :controller
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"session" => %{"username" => username, "password" => pass}}) do
+    case Rumble.Accounts.authenticate_by_username_and_pass(username, pass) do
+      {:ok, user} ->
+        conn
+        |> RumbleWeb.Auth.login(user)
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Invalid username/password combination")
+        |> render("new.html")
+    end
+  end
+
+  def delete(conn, _params) do
+    conn
+    |> RumbleWeb.Auth.logout()
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
+end
